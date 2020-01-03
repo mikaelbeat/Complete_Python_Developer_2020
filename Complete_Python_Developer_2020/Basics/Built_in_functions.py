b45fd98f7d60cc9491c65e19870f11f04ba17731
ryynanenphm <petri.ryynanen@tieto.com>
2020-01-03 15:59:59 +0200

    Update

---
+++ Complete_Python_Developer_2020/Basics/Built_in_functions.py
@@ -0,0 +1,15 @@
+
+
+data = "it\'s a great weather!"
+
+# Length of the data
+print(f"Data sting length is {len(data)}.")
+
+# Capitalizes string
+print(data.capitalize())
+
+# Search data for given value, return start index
+print(data.find("great"))
+
+# Replaces values with given value
+print(data.replace("great", "terrible"))
