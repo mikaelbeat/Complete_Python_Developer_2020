e7e5671acff8189cb235a48a66d36309f65b5fb8
mikaelbeat <ryynanenphm@gmail.com>
2020-02-02 21:39:09 +0200

    Update

--- Complete_Python_Developer_2020/Functional_programming/Reduce.py
+++ Complete_Python_Developer_2020/Functional_programming/Reduce.py
@@ -1,4 +1,8 @@
 
+# Reduce is a really useful function for performing some computation on a list and returning the result. 
+# It applies a rolling computation to sequential pairs of values in a list. 
+# For example, if you wanted to compute the product of a list of integers.
+
 from functools import reduce
 
 data1 = [1, 2, 3, 4, 5]
