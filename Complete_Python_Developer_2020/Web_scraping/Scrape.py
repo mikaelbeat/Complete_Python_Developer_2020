aadd9dcb3627606e70158182e50edc8f6fcdd4ff
mikaelbeat <ryynanenphm@gmail.com>
2020-03-13 11:07:30 +0200

    Update

--- Complete_Python_Developer_2020/Web_scraping/Scrape.py
+++ Complete_Python_Developer_2020/Web_scraping/Scrape.py
@@ -1,14 +1,37 @@
 
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get("https://news.ycombinator.com/news")
 
 soup = BeautifulSoup(res.text, "html.parser")
 
 #print(soup.body)
+#print(soup.find_all("div"))
+#print(soup.title)
+#print(soup.find(id="score_22561291"))
+#print(soup.select(".score"))
+#print(soup.select("#score_22564142"))
 
-print(soup.find_all("div"))
+
+links = soup.select(".storylink")
+subtext = soup.select(".subtext")
+#print(votes[0])
+
+def create_custom_list(links, subtext):
+    data = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get("href", None)
+        vote = subtext[idx].select(".score")
+        if len(vote):
+            points = int(vote[0].getText().replace(" points", ""))
+            if points > 99:
+                data.append({"title": title, "link": href, "votes": points})
+    return data
 
 
-print(soup.find(""))
+pprint.pprint(create_custom_list(links, subtext))
+
+#create_custom_list(links, subtext)
