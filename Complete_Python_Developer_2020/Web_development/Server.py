46542a5c5283eaae5274c3433fb1a9bafc95dab7
mikaelbeat <ryynanenphm@gmail.com>
2020-03-25 16:35:26 +0200

    Update

--- Complete_Python_Developer_2020/Web_development/Server.py
+++ Complete_Python_Developer_2020/Web_development/Server.py
@@ -13,7 +13,7 @@
 # flask run
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -27,3 +27,12 @@ def my_home():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        return "Something went wrong. Try Again!"
