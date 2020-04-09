1292e9c2bda3e22833185bef6e151030e8a8266f
mikaelbeat <ryynanenphm@gmail.com>
2020-04-09 15:06:13 +0300

    Update

--- Complete_Python_Developer_2020/FTP/FTP_training.py
+++ Complete_Python_Developer_2020/FTP/FTP_training.py
@@ -2,5 +2,5 @@
 from ftplib import FTP
 
 ftp = FTP("ftp.dlptest.com")
-ftp.login(user="dlpuser@dlptest.com", passwd ="SzMf7rTE4pCrf9dV286GuNe4N")
+ftp.login(user="dlpuser@dlptest.com", passwd="SzMf7rTE4pCrf9dV286GuNe4N")
 ftp.dir()
