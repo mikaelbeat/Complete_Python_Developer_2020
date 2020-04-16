1055b4bf53bdf5ccf48bb61f4fd52d50c49f4677
mikaelbeat <ryynanenphm@gmail.com>
2020-04-16 23:04:52 +0300

    Update

---
+++ Complete_Python_Developer_2020/Test_automation/Automation.py
@@ -0,0 +1,6 @@
+
+from selenium import webdriver
+
+chrome_browser = webdriver.Chrome()
+
+chrome_browser.maximize_window()
--- Complete_Python_Developer_2020/Web_development/Server.py
+++ Complete_Python_Developer_2020/Web_development/Server.py
@@ -14,6 +14,7 @@
 
 
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -28,11 +29,32 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open("database.txt", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n{email}, {subject}, {message}")
+
+def write_to_csv_file(data):
+    with open("database.csv", newline="", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database, delimiter=",",  quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == "POST":
-        data = request.form.to_dict()
-        print(data)
-        return redirect("/thankyou.html")
+        try:
+            data = request.form.to_dict()
+            print(data)
+            write_to_file(data)
+            write_to_csv_file(data)
+            return redirect("/thankyou.html")
+        except:
+            return "Error while saving to database!"
     else:
         return "Something went wrong. Try Again!"
